923e6cebe192d9b78039ec2c8381c763c64abbb0
Aryanjain7801 <jainn.aryan.2004@gmail.com>
2026-03-11 14:13:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (43.50%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,61 @@
+class Solution {
+
+    //rotation kaha hui check
+
+    public static int FindMin(int low , int high , int[] nums)
+    {
+        while(low < high)
+        {
+            int mid = low + (high-low)/2 ;
+            if(nums[mid] > nums[high])
+            {
+                low = mid + 1 ;
+            }
+            else
+            {
+                high = mid ;
+            }
+        }
+
+        return low ;
+    }
+    //target kaha hai check
+
+    public static int BinarySearch(int low , int high , int[] nums , int target)
+    {
+        while(low <= high)
+        {
+            int mid = low + (high-low)/2 ;
+
+            if(nums[mid] == target)
+            {
+                return mid ;
+            }
+            else if(nums[mid] < target)
+            {
+                low = mid + 1 ;
+            }
+            else
+            {
+                high = mid-1 ;
+            }
+        }
+        return -1 ;
+    }
+    //dono k agar mila toh do vrna -1
+    public int search(int[] nums, int target) {
+        
+        int n = nums.length ;
+
+        int index = FindMin(0,n-1,nums) ;
+        int ans = BinarySearch(0,index-1,nums,target) ;
+
+        if(ans != -1)
+        {
+            return ans ;
+        }
+
+        ans = BinarySearch(index,n-1, nums,target) ;
+        return ans ;
+    }
+}
