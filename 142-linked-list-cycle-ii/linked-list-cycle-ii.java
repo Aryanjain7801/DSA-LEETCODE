2ae85f623faa0920001d5a8795f7875e771b4e57
Aryanjain7801 <jainn.aryan.2004@gmail.com>
2026-04-10 12:18:23 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (57.51%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -1,26 +1,47 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-public class Solution {
-    public ListNode detectCycle(ListNode head) {
+// /**
+//  * Definition for singly-linked list.
+//  * class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode(int x) {
+//  *         val = x;
+//  *         next = null;
+//  *     }
+//  * }
+//  */
+// public class Solution {
+//     public ListNode detectCycle(ListNode head) {
    
-    HashSet<ListNode> set = new HashSet<>();
-    ListNode temp = head;
-    while(temp != null){
-        if(set.contains(temp)){
-            return temp;
+//     HashSet<ListNode> set = new HashSet<>();
+//     ListNode temp = head;
+//     while(temp != null){
+//         if(set.contains(temp)){
+//             return temp;
+//         }
+//         set.add(temp);
+//         temp = temp.next;
+//     }
+//     return null;
+//     }
+// }
+
+public class Solution {
+     public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        
+        if(slow == fast){
+            ListNode curr = head;
+            while (curr != slow){
+                slow = slow.next;
+                curr = curr.next;
+            }
+            return curr;
         }
-        set.add(temp);
-        temp = temp.next;
-    }
-    return null;
-    }
+        }
+        return null; 
+     }
 }
